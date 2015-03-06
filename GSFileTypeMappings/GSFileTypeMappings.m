ab99377d7bc437a7ecbff19499e22bf09187a562
Goichi Hirakawa <gooichi@gyazsquare.com>
2015-03-06 14:48:19 +0900

    Add \@synchronized

--- GSFileTypeMappings/GSFileTypeMappings.m
+++ GSFileTypeMappings/GSFileTypeMappings.m
@@ -16,55 +16,54 @@
 
 @implementation GSFileTypeMappings
 
-+ (void)initialize {
-    CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, CFSTR("txt"), NULL);
-    if (UTI) {
-        CFRelease(UTI);
-    }
-}
-
 GSSynthesizeSingleton(GS, FileTypeMappings);
 
 - (NSArray *)extensionsForMIMEType:(NSString *)MIMEType {
-    CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, (__bridge CFStringRef)MIMEType, NULL);
-    if (!UTI) {
-        return nil;
-    }
-    CFArrayRef extensions;
-    if (UTTypeCopyAllTagsWithClass) {
-        // OS X 10.10 / iOS 8.0 or later
-        extensions = UTTypeCopyAllTagsWithClass(UTI, kUTTagClassFilenameExtension);
-    } else {
-        // OS X 10.9.x / iOS 7.x or earlier
-        CFStringRef extension = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassFilenameExtension);
-        CFIndex numValues = extension ? 1 : 0;
-        extensions = CFArrayCreate(NULL, (void *)&extension, numValues, &kCFTypeArrayCallBacks);
-        if (extension) {
-            CFRelease(extension);
+    @synchronized(self) {
+        CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, (__bridge CFStringRef)MIMEType, NULL);
+        if (!UTI) {
+            return nil;
         }
+        CFArrayRef extensions;
+        if (UTTypeCopyAllTagsWithClass) {
+            // OS X 10.10 / iOS 8.0 or later
+            extensions = UTTypeCopyAllTagsWithClass(UTI, kUTTagClassFilenameExtension);
+        } else {
+            // OS X 10.9.x / iOS 7.x or earlier
+            CFStringRef extension = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassFilenameExtension);
+            CFIndex numValues = extension ? 1 : 0;
+            extensions = CFArrayCreate(NULL, (void *)&extension, numValues, &kCFTypeArrayCallBacks);
+            if (extension) {
+                CFRelease(extension);
+            }
+        }
+        CFRelease(UTI);
+        return (__bridge_transfer NSArray *)extensions;
     }
-    CFRelease(UTI);
-    return (__bridge_transfer NSArray *)extensions;
 }
 
 - (NSString *)preferredExtensionForMIMEType:(NSString *)MIMEType {
-    CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, (__bridge CFStringRef)MIMEType, NULL);
-    if (!UTI) {
-        return nil;
+    @synchronized(self) {
+        CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, (__bridge CFStringRef)MIMEType, NULL);
+        if (!UTI) {
+            return nil;
+        }
+        CFStringRef extension = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassFilenameExtension);
+        CFRelease(UTI);
+        return (__bridge_transfer NSString *)extension;
     }
-    CFStringRef extension = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassFilenameExtension);
-    CFRelease(UTI);
-    return (__bridge_transfer NSString *)extension;
 }
 
 - (NSString *)MIMETypeForExtension:(NSString *)extension {
-    CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)extension, NULL);
-    if (!UTI) {
-        return nil;
+    @synchronized(self) {
+        CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)extension, NULL);
+        if (!UTI) {
+            return nil;
+        }
+        CFStringRef MIMEType = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassMIMEType);
+        CFRelease(UTI);
+        return (__bridge_transfer NSString *)MIMEType;
     }
-    CFStringRef MIMEType = UTTypeCopyPreferredTagWithClass(UTI, kUTTagClassMIMEType);
-    CFRelease(UTI);
-    return (__bridge_transfer NSString *)MIMEType;
 }
 
 @end
